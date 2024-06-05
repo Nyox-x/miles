ac79f4a7ae626da73822cef279a2a0a4229b0824
Nyox-x <62241081+Nyox-x@users.noreply.github.com>
2024-06-05 09:17:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as tick
 from matplotlib.offsetbox import AnnotationBbox, OffsetImage
 
-RUNNER = "NAOSENSE"
+RUNNER = "Kith"
 
 T = TypeVar("T")
 K = TypeVar("K")
